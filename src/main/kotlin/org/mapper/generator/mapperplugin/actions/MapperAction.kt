68f4a7d733689f5afa46305c87b89184ca4a4aa7
Anton Shcherbakov <1mailanton@gmail.com>
2024-12-18 11:29:04 +0100

    create swing form

---
+++ src/main/kotlin/org/mapper/generator/mapperplugin/actions/MapperAction.kt
@@ -0,0 +1,22 @@
+package org.mapper.generator.mapperplugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+import org.mapper.generator.mapperplugin.actions.ui.MapperDialog
+
+class MapperAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        MapperDialog().onShow { result ->
+            Messages.showInfoMessage(
+                GENERATION_RESULT + result,
+                TITLE
+            )
+        }
+    }
+
+    companion object Constant {
+        private const val TITLE = "Mapper Generator"
+        private const val GENERATION_RESULT = "Generation result: "
+    }
+}
---
+++ src/main/kotlin/org/mapper/generator/mapperplugin/actions/ui/MapperDialog.kt
@@ -0,0 +1,43 @@
+package org.mapper.generator.mapperplugin.actions.ui
+
+import com.intellij.openapi.ui.DialogWrapper
+import javax.swing.*
+
+class MapperDialog : DialogWrapper(true) {
+
+    private val inputField1 = JTextField()
+    private val inputField2 = JTextField()
+    private val message1 = JLabel(MESSAGE_FILE)
+    private val message2 = JLabel(MESSAGE_FOLDER)
+
+    init {
+        title = TITLE
+        init()
+    }
+
+    override fun createCenterPanel(): JComponent {
+        val panel = JPanel()
+        panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
+        panel.add(message1)
+        panel.add(inputField1)
+        panel.add(Box.createVerticalStrut(10))
+        panel.add(message2)
+        panel.add(inputField2)
+        return panel
+    }
+
+    fun onShow(action: (result: String) -> Unit)  {
+        if (showAndGet()) {
+            action(
+//                result
+                "Success"
+            )
+        }
+    }
+
+    companion object Constant {
+        private const val TITLE = "Mapper Generator"
+        private const val MESSAGE_FILE = "Set your file settings:"
+        private const val MESSAGE_FOLDER = "Set your folder for generating code:"
+    }
+}
