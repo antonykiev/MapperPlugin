2a4617af4ef2ac6f7a8de7831dd5395714623e3e
Anton Shcherbakov <1mailanton@gmail.com>
2024-12-20 19:34:16 +0100

    clean code

--- src/main/kotlin/org/mapper/generator/mapperplugin/buisness/generation/GetClassNameFromFullNameUseCase.kt
+++ src/main/kotlin/org/mapper/generator/mapperplugin/buisness/generation/GetClassNameFromFullNameUseCase.kt
@@ -3,6 +3,7 @@ package org.mapper.generator.mapperplugin.buisness.generation
 class GetClassNameFromFullNameUseCase {
 
     operator fun invoke(fullClassName: String): String {
-        return fullClassName.substring(fullClassName.lastIndexOf(".") + 1)
+        return fullClassName
+            .substring(startIndex = fullClassName.lastIndexOf(".") + 1)
     }
 }
