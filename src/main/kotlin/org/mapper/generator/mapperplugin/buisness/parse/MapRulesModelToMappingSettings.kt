2983dfc62e18ee77dc733f423674fee8cf521c4f
Anton Shcherbakov <1mailanton@gmail.com>
2024-12-23 23:05:26 +0100

    clean code

--- src/main/kotlin/org/mapper/generator/mapperplugin/buisness/parse/MapRulesModelToMappingSettings.kt
+++ src/main/kotlin/org/mapper/generator/mapperplugin/buisness/parse/MapRulesModelToMappingSettings.kt
@@ -40,6 +40,5 @@ class MapRulesModelToMappingSettings {
                 )
             }
         ) }
-
     }
 }
--- src/main/kotlin/org/mapper/generator/mapperplugin/buisness/parse/TextFileUseCase.kt
+++ src/main/kotlin/org/mapper/generator/mapperplugin/buisness/parse/TextFileUseCase.kt
@@ -11,36 +11,29 @@ import java.nio.charset.StandardCharsets
 class TextFileUseCase {
 
     operator fun invoke(
-        project: Project,
         filePath: String
     ): Result<String> {
         return runCatching {
             val virtualFile = createVirtualFile(filePath)
             return@runCatching readFileContentWithUnsavedChanges(virtualFile).also { println(it) }
-                ?: throw Exception("File is empty")
         }
     }
 
-    private fun createVirtualFile(path: String): VirtualFile? {
+    private fun createVirtualFile(path: String): VirtualFile {
         val ioFile = File(path)
-
-        // Refresh the local file system to make the new file visible to the VFS
         val localFileSystem = LocalFileSystem.getInstance()
         localFileSystem.refreshAndFindFileByIoFile(ioFile)
         localFileSystem.refresh(false)
-
-        // Find the VirtualFile in the VFS
-        return localFileSystem.findFileByIoFile(ioFile)
+        return localFileSystem.findFileByIoFile(ioFile) ?: throw Exception("File not found")
     }
 
-    private fun readFileContentWithUnsavedChanges(virtualFile: VirtualFile?): String? {
-        virtualFile ?: return null
+    private fun readFileContentWithUnsavedChanges(virtualFile: VirtualFile): String {
         val fileDocumentManager = FileDocumentManager.getInstance()
         val document = fileDocumentManager.getDocument(virtualFile)
         return if (document != null && fileDocumentManager.isFileModified(virtualFile)) {
-            document.text // Use unsaved changes
+            document.text
         } else {
-            virtualFile.contentsToByteArray().toString(StandardCharsets.UTF_8) // Use VFS content
+            virtualFile.contentsToByteArray().toString(StandardCharsets.UTF_8)
         }
     }
 }
