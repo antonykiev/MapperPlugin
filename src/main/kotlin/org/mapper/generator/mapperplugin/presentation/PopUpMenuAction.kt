4773c69235648740d95f57dd1399438c197020b1
Anton Shcherbakov <1mailanton@gmail.com>
2024-12-25 16:15:45 +0100

    add popup menu

---
+++ src/main/kotlin/org/mapper/generator/mapperplugin/presentation/PopUpMenuAction.kt
@@ -0,0 +1,11 @@
+package org.mapper.generator.mapperplugin.presentation
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class PopUpMenuAction: AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
